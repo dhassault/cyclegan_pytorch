dadd22cc104c660f2af1647d2b0a7a6568eb0427
yann <yann.leguilly@gmail.com>
2021-04-18 23:49:15 +0900

    feat: implement residual block module

---
+++ cyclegan_pytorch/architecture/residual_block.py
@@ -0,0 +1,23 @@
+"""
+This is used several times
+in the generator.
+"""
+from torch import nn
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_features):
+        super(ResidualBlock, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_features, in_features, 3),
+            nn.InstanceNorm2d(in_features),
+            nn.ReLU(inplace=True),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_features, in_features, 3),
+            nn.InstanceNorm2d(in_features),
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
