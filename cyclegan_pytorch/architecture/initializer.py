3cc627cacaf9e4b51edb3f45f7dc3a67c19f5e50
yann <yann.leguilly@gmail.com>
2021-04-18 23:48:47 +0900

    feat: implement gaussian weight inti

---
+++ cyclegan_pytorch/architecture/initializer.py
@@ -0,0 +1,18 @@
+"""
+From the paper, Appendix, 7.1 Training details:
+"Weights are initialized from aGaussian distribution
+N(0,0.02)."
+"""
+
+import torch
+
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+        if hasattr(m, "bias") and m.bias is not None:
+            torch.nn.init.constant_(m.bias.data, 0.0)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
